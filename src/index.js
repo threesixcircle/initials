638e4e192742f6ac546d89abef2469643c6909b8
Robert Lyall <robert.g.lyall@gmail.com>
2021-04-14 09:39:23 +0100

    change font to proxima nova

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const path = require("path");
 
 const app = express();
 
+const colors = ["#ffc300", "#ff5733", "#c70039", "#900c3f", "#581845"];
+
+const random = (collection) => {
+  return collection[Math.floor(Math.random() * collection.length)];
+};
+
 const checkPresence = (path) => {
   return new Promise((resolve, reject) => {
     try {
@@ -24,27 +30,28 @@ const generate = ({ initials }) => {
     try {
       const hash = md5(initials);
       const file = `avatar-${hash}.png`;
+      const filepath = path.resolve("data", file);
 
-      checkPresence(path.resolve("data", file))
+      checkPresence(filepath)
         .then((present) => {
           if (present) {
             console.log("Already generated. Returning.");
-            return resolve(file);
+            return resolve(filepath);
           }
 
-          gm(512, 512, "#f62f62")
-            .font(path.resolve("fonts", "Effra.ttf"), 256)
+          gm(512, 512, random(colors))
+            .font(path.resolve("fonts", "Proxima-Nova.ttf"), 256)
             .fill("#ffffff")
             .stroke("transparent")
             .drawText(0, 0, initials, "Center")
             .quality(100)
-            .write(path.resolve("data", file), (error) => {
+            .write(filepath, (error) => {
               if (error) {
                 console.error(error);
                 return reject(error);
               }
 
-              resolve(file);
+              resolve(filepath);
             });
         })
         .catch(reject);
@@ -61,8 +68,8 @@ app.get("/", (req, res) => {
   const { initials } = query;
 
   generate({ initials })
-    .then(() => {
-      res.sendStatus(200);
+    .then((filepath) => {
+      res.status(200).sendFile(filepath);
     })
     .catch(() => {
       res.sendStatus(500);
